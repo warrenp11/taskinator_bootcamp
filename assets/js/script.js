fdb63fa45a435775c47d25d5e03379c56d5d4ff3
Peter <warrenp11@gmail.com>
2022-04-07 01:51:50 -0400

    Prevent browser window from reloading after button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
 // console.dir(window.document);
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+  //  Prevents the browser window from automatically refreshing
+  event.preventDefault();
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task";
   tasksToDoEl.appendChild(listItemEl);
+
+//   console.log(event);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
